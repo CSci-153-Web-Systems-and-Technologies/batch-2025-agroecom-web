9f214e7d2d00ec5d45c945de3438babd42f0f095
DonyoBen <saquinbenjie7@gmail.com>
2025-12-09 07:25:07 +0800

    add lender profile

---
+++ app/(main)/dashboard/lender/profile/page.tsx
@@ -0,0 +1,21 @@
+import PersonalInfo from '../../components/PersonalInfo'
+import AccountInformation from '../../components/AccountInformation'
+import ChangePassword from '../../components/ChangePassword'
+
+
+export default function LenderProfile() {
+    return (
+      <div className="p-6 grid grid-cols-1 lg:grid-cols-3 gap-6 w-full max-w-7xl mx-auto">
+  
+        <div className="lg:col-span-2 space-y-6">
+          <PersonalInfo />
+          <AccountInformation />
+        </div>
+  
+        <div className="lg:col-span-1">
+          <ChangePassword />
+        </div>
+  
+      </div>
+    );
+}
